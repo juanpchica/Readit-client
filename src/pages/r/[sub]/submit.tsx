784f0133fd7201242c0d41ccc902839054a5d009
juanpchica <juanpchica@hotmail.com>
2021-05-11 08:59:53 -0500

    Submit post getServerSideProps commented

--- src/pages/r/[sub]/submit.tsx
+++ src/pages/r/[sub]/submit.tsx
@@ -85,15 +85,15 @@ export default function submit() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  try {
-    const cookie = req.headers.cookie;
-    if (!cookie) throw new Error("Missing auth token cookie");
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   try {
+//     const cookie = req.headers.cookie;
+//     if (!cookie) throw new Error("Missing auth token cookie");
 
-    await Axios.get("/auth/me", { headers: { cookie } });
+//     await Axios.get("/auth/me", { headers: { cookie } });
 
-    return { props: {} };
-  } catch (err) {
-    res.writeHead(307, { Location: "/login" }).end();
-  }
-};
+//     return { props: {} };
+//   } catch (err) {
+//     res.writeHead(307, { Location: "/login" }).end();
+//   }
+// };
